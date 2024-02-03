5643e7e338ba9276ffd5de1cb1cc531c82cbc4dd
Richard Blažek <blazek.risa@gmail.com>
2024-02-03 23:43:51 +0100

    Removed the test file

--- utils.asm
+++ utils.asm
@@ -1,11 +1,9 @@
-format ELF executable
-entry  _start
-
-segment readable executable
-
-; converts positive integer to string
-; subroutine itoa (number: dword, destination: dword) -> (length : dword)
-itoa:
+; Converts a positive integer to a string.
+; Arguments:
+;   - number
+;   - destination
+; Returns: the length of the string
+utils.itoa:
     push ebx            ; push registers
     push ecx
     push edx
@@ -47,26 +45,3 @@ itoa:
     pop ecx
     pop ebx
     ret
-
-
-_start:
-    mov edi,msg
-    add edi,14
-    push edi
-    push 123456789
-    call itoa
-    add esp, 8
-
-    mov edx,len     ; third argument: message length
-    mov ecx,msg     ; second argument: pointer to message to write
-    mov ebx,1       ; first argument: file handle (stdout)
-    mov eax,4       ; system call number (sys_write)
-    int 0x80        ; call kernel
-
-  	mov ebx,0       ; first syscall argument: exit code
-    mov eax,1       ; system call number (sys_exit)
-    int 0x80        ; call kernel
-
-segment readable writable
-    msg: db "Hello, world!            ",10
-    len = $ - msg
